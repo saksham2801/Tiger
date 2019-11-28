832a6e57fe6625d20d7377768ccfce00fc9356af
Saksham Seth <sethsaks@amazon.com>
2019-11-28 17:08:13 +0530

    added login page

--- app/src/main/java/com/example/tiger/MainActivity.java
+++ app/src/main/java/com/example/tiger/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.tiger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.splashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
