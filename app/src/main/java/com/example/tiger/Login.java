f9364816364f63673017b9ff741fb66c706e4eac
Saksham Seth <sethsaks@amazon.com>
2019-11-29 14:17:12 +0530

    skipping login page for now

--- app/src/main/java/com/example/tiger/Login.java
+++ app/src/main/java/com/example/tiger/Login.java
@@ -2,11 +2,7 @@ package com.example.tiger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
-
-import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
 
@@ -14,13 +10,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
     }
-
+/*
     private int[] screenSize() {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         return new int[]{size.x, size.y};
     }
+ */
 }
--- app/src/main/java/com/example/tiger/MainActivity.java
+++ app/src/main/java/com/example/tiger/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(MainActivity.this, Login.class);
+                Intent i=new Intent(MainActivity.this, LandingPage.class);
                 startActivity(i);
                 finish();
             }
